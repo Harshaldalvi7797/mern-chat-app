74095c800a911ef65cf5506ab03ce610fc24b4fd
Harshaldalvi7797 <dalviharshal7797@gmail.com>
2022-04-21 18:05:33 +0530

    signup form ui done

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,7 +1,89 @@
-import React from 'react'
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { VStack } from "@chakra-ui/layout";
+import { useToast } from "@chakra-ui/toast";
+import axios from "axios";
+import { useState } from "react";
+import { useHistory } from "react-router";
 
-export default function Signup() {
-  return (
-    <div>Signup</div>
-  )
+const  Signup=()=> {
+    const [show, setShow] = useState(false);
+    const handleClick = () => setShow(!show);
+    const toast = useToast();
+    const history = useHistory();
+  
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [confirmpassword, setConfirmpassword] = useState();
+    const [password, setPassword] = useState();
+    const [pic, setPic] = useState();
+    const [picLoading, setPicLoading] = useState(false);
+    return (
+        <VStack spacing="5px">
+            <FormControl id="first-name" isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input
+                    placeholder="Enter Your Name"
+
+                />
+            </FormControl>
+            <FormControl id="email" isRequired>
+                <FormLabel>Email Address</FormLabel>
+                <Input
+                    type="email"
+                    placeholder="Enter Your Email Address"
+
+                />
+            </FormControl>
+            <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup size="md">
+                    <Input
+                        type={"password"}
+                        placeholder="Enter Password"
+
+                    />
+                    <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" >
+                            Hide
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+            <FormControl id="password" isRequired>
+                <FormLabel>Confirm Password</FormLabel>
+                <InputGroup size="md">
+                    <Input
+                        type={"password"}
+                        placeholder="Confirm password"
+
+                    />
+                    <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" >
+
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+            <FormControl id="pic">
+                <FormLabel>Upload your Picture</FormLabel>
+                <Input
+                    type="file"
+                    p={1.5}
+                    accept="image/*"
+
+                />
+            </FormControl>
+            <Button
+                colorScheme="blue"
+                width="100%"
+                style={{ marginTop: 15 }}
+
+            >
+                Sign Up
+            </Button>
+        </VStack>
+    )
 }
+export default  Signup
