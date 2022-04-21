80f7e865615733b7bdd5d7b1d5dbed5535890a72
Harshaldalvi7797 <dalviharshal7797@gmail.com>
2022-04-21 18:05:49 +0530

    login form ui done

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,7 +1,65 @@
-import React from 'react'
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { VStack } from "@chakra-ui/layout";
+import { useState } from "react";
+import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
-export default function Login() {
+ const  Login=()=> {
+    const [show, setShow] = useState(false);
+    const handleClick = () => setShow(!show);
+    const toast = useToast();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [loading, setLoading] = useState(false);
+  
+    const history = useHistory();
   return (
-    <div>Login</div>
+    <VStack spacing="10px">
+    <FormControl id="email" isRequired>
+      <FormLabel>Email Address</FormLabel>
+      <Input
+        value={email}
+        type="email"
+        placeholder="Enter Your Email Address"
+        // onChange={(e) => setEmail(e.target.value)}
+      />
+    </FormControl>
+    <FormControl id="password" isRequired>
+      <FormLabel>Password</FormLabel>
+      <InputGroup size="md">
+        <Input
+          value={password}
+        //   onChange={(e) => setPassword(e.target.value)}
+          type={show ? "text" : "password"}
+          placeholder="Enter password"
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" >
+            {/* {show ? "Hide" : "Show"} */}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    </FormControl>
+    <Button
+      colorScheme="blue"
+      width="100%"
+      style={{ marginTop: 15 }}
+
+    >
+      Login
+    </Button>
+    <Button
+      variant="solid"
+      colorScheme="red"
+      width="100%"
+
+    >
+      Get Guest User Credentials
+    </Button>
+  </VStack>
   )
 }
+export default Login
