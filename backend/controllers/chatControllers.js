54cb84f557dbf3ce6caf4543858eeffcf109af9e
Harshaldalvi7797 <dalviharshal7797@gmail.com>
2022-04-23 16:47:59 +0530

    rename griup and remove group api

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -99,9 +99,53 @@ const createGroupChat = asyncHandler(async (req, res) => {
         throw new Error(error.message);
     }
 });
-
-
+const renameGroup = asyncHandler(async (req, res) => {
+    const { chatId, chatName } = req.body;
+  
+    const updatedChat = await Chat.findByIdAndUpdate(
+      chatId,
+      {
+        chatName: chatName,
+      },
+      {
+        new: true,
+      }
+    )
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+  
+    if (!updatedChat) {
+      res.status(404);
+      throw new Error("Chat Not Found");
+    } else {
+      res.json(updatedChat);
+    }
+  })
+  const removeFromGroup = asyncHandler(async (req, res) => {
+    const { chatId, userId } = req.body;
+  
+    // check if the requester is admin
+  
+    const removed = await Chat.findByIdAndUpdate(
+      chatId,
+      {
+        $pull: { users: userId },
+      },
+      {
+        new: true,
+      }
+    )
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+  
+    if (!removed) {
+      res.status(404);
+      throw new Error("Chat Not Found");
+    } else {
+      res.json(removed);
+    }
+  });
 module.exports = {
-    accessChat, fetchChats, createGroupChat
+    accessChat, fetchChats, createGroupChat,renameGroup,removeFromGroup
 
 };
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { accessChat, fetchChats, createGroupChat
+const { accessChat, fetchChats, createGroupChat,renameGroup,removeFromGroup
 
 } = require("../controllers/chatControllers");
 const { protect } = require("../middleware/authMiddleware");
@@ -9,8 +9,8 @@ const router = express.Router();
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/groupremove").put(protect, removeFromGroup);
+router.route("/rename").put(protect, renameGroup);
+ router.route("/groupremove").put(protect, removeFromGroup);
 // router.route("/groupadd").put(protect, addToGroup);
 
 module.exports = router;
