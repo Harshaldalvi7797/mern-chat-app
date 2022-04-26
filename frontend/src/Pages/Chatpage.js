1c0bbff99b1991e9614f43023c2a18b620019b85
Harshaldalvi7797 <dalviharshal7797@gmail.com>
2022-04-26 14:25:40 +0530

    search integration done

--- frontend/src/Pages/Chatpage.js
+++ frontend/src/Pages/Chatpage.js
@@ -11,7 +11,7 @@ const Chatpage = () => {
 
   return (
     <div style={{ width: "100%" }}>
-      {user && <SideDrawer />}
+      {user && <SideDrawer user={user} />}
       {/* <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
         {user && <MyChats fetchAgain={fetchAgain} />}
         {user && (
--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -8,17 +8,89 @@ import { Box, Text } from "@chakra-ui/layout";
 import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { Tooltip } from "@chakra-ui/tooltip";
 import { Avatar } from "@chakra-ui/avatar";
+import axios from "axios";
 import {
+    Drawer,
+    DrawerBody,
+    DrawerContent,
+    DrawerHeader,
+    DrawerOverlay,
+} from "@chakra-ui/modal";
+import ProfileModal from './ProfileModal';
+import { ChatState } from "../../Context/ChatProvider";
+import {
+
     Menu,
     MenuButton,
     MenuDivider,
     MenuItem,
     MenuList,
 } from "@chakra-ui/menu";
+import ChatLoading from '../ChatLoading';
+import UserListItem from './../userAvatar/UserListItem';
 const SideDrawer = () => {
+    const [search, setSearch] = useState("");
+    const [searchResult, setSearchResult] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [loadingChat, setLoadingChat] = useState(false);
+
     const toast = useToast();
     const { isOpen, onOpen, onClose } = useDisclosure();
     const history = useHistory();
+    const logoutHandler = () => {
+        localStorage.removeItem("userInfo");
+        history.push("/");
+    };
+
+    const handleSearch = async () => {
+        if (!search) {
+            toast({
+                title: "Please Enter something in search",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "top-left",
+            });
+            return;
+        }
+
+
+        try {
+            setLoading(true);
+
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                },
+            };
+
+            const { data } = await axios.get(`/api/user?search=${search}`, config);
+
+            setLoading(false);
+            setSearchResult(data);
+        }
+        catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: "Failed to Load the Search Results",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom-left",
+            });
+        }
+    }
+    const accessChat = async (userId) => {
+        console.log(userId);
+    }
+    const {
+        setSelectedChat,
+        user,
+        notification,
+        setNotification,
+        chats,
+        setChats,
+    } = ChatState()
     return (
         <React.Fragment>
             <Box
@@ -58,16 +130,48 @@ const SideDrawer = () => {
                             />
                         </MenuButton>
                         <MenuList>
-                            {/* <ProfileModal> */}
-                            <MenuItem>My Profile</MenuItem>
-                            <MenuItem>Logout</MenuItem>
-                            {/* </ProfileModal> */}
+                            <ProfileModal user={user}>
+                                {/* <MenuItem>My Profile</MenuItem> */}
+
+                            </ProfileModal>
                             <MenuDivider />
-                            {/* <MenuItem onClick={logoutHandler}>Logout</MenuItem> */}
+
+                            <MenuItem onClick={logoutHandler}>Logout</MenuItem>
                         </MenuList>
                     </Menu>
                 </div>
             </Box>
+            <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+                <DrawerOverlay />
+                <DrawerContent>
+                    <DrawerHeader borderBottomWidth="1px">Search Users</DrawerHeader>
+                    <DrawerBody>
+                        <Box d="flex" pb={2}>
+                            <Input
+                                placeholder="Search by name or email"
+                                mr={2}
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
+                            />
+                            <Button onClick={handleSearch}>Go</Button>
+                        </Box>
+                        {loading ? (
+                            <ChatLoading />
+                        ) : (
+                            searchResult?.map((user) => (
+                                // <h1>results</h1>
+                                <UserListItem
+                                    key={user._id}
+                                    user={user}
+                                    handleFunction={() => accessChat(user._id)}
+                                />
+                            ))
+                        )
+                        }
+                        {/* {loadingChat && <Spinner ml="auto" d="flex" />} */}
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
         </React.Fragment>
     )
 }
