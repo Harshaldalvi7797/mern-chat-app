5e20ccadd6153f1da32dde6f58bef037c29b4f6b
Harshaldalvi7797 <dalviharshal7797@gmail.com>
2022-04-28 17:11:48 +0530

    group chat created

--- frontend/src/components/miscellaneous/GroupChatModal.js
+++ frontend/src/components/miscellaneous/GroupChatModal.js
@@ -77,7 +77,51 @@ const GroupChatModal = ({ children }) => {
     const handleDelete = (delUser) => {
         setSelectedUsers(selectedUsers.filter((sel) => sel._id !== delUser._id));
     };
-    const handleSubmit = async () => { }
+    const handleSubmit = async () => {
+        if (!groupChatName || !selectedUsers) {
+            toast({
+              title: "Please fill all the feilds",
+              status: "warning",
+              duration: 5000,
+              isClosable: true,
+              position: "top",
+            });
+            return;
+          }
+        try {
+            const config = {
+              headers: {
+                Authorization: `Bearer ${user.token}`,
+              },
+            };
+            const { data } = await axios.post(
+              `/api/chat/group`,
+              {
+                name: groupChatName,
+                users: JSON.stringify(selectedUsers.map((u) => u._id)),
+              },
+              config
+            );
+            setChats([data, ...chats]);
+            onClose();
+            toast({
+              title: "New Group Chat Created!",
+              status: "success",
+              duration: 5000,
+              isClosable: true,
+              position: "bottom",
+            });
+          } catch (error) {
+            toast({
+              title: "Failed to Create the Chat!",
+              description: error.response.data,
+              status: "error",
+              duration: 5000,
+              isClosable: true,
+              position: "bottom",
+            });
+          }
+     }
 
     return (
         <>
@@ -115,7 +159,7 @@ const GroupChatModal = ({ children }) => {
                                 <UserBadgeItem
                                     key={u._id}
                                     user={u}
-
+                                    handleFunction={() => handleDelete(u)}
                                 />
                             ))}
                         </Box>
