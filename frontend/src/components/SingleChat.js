f71d54dedcefb1511919276c88fb5a5e467b59e0
Harshaldalvi7797 <dalviharshal7797@gmail.com>
2022-04-29 00:55:55 +0530

    update group chat

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -8,11 +8,12 @@ import { getSender, getSenderFull } from "../config/ChatLogics";
 
 import axios from "axios";
 import { ArrowBackIcon } from "@chakra-ui/icons";
-// import ProfileModal from "./miscellaneous/ProfileModal";
+import ProfileModal from "./miscellaneous/ProfileModal";
 // import ScrollableChat from "./ScrollableChat";
 // import Lottie from "react-lottie";
 // import animationData from "../animations/typing.json"
 import { ChatState } from "../Context/ChatProvider";
+import UpdateGroupChatModal from './miscellaneous/UpdateGroupChatModal';
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
     const { selectedChat, setSelectedChat, user, notification, setNotification } =
         ChatState();
@@ -37,21 +38,22 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                         (!selectedChat.isGroupChat ? (
                             <>
                                 {getSender(user, selectedChat.users)}
-                                {/* <ProfileModal
+                                <ProfileModal
                                     user={getSenderFull(user, selectedChat.users)}
-                                /> */}
+                                />
                             </>
                         ) : (
                             <>
                                 {selectedChat.chatName.toUpperCase()}
-                                {/* <UpdateGroupChatModal
-                                    fetchMessages={fetchMessages}
+                                <UpdateGroupChatModal
+                                    // fetchMessages={fetchMessages}
                                     fetchAgain={fetchAgain}
                                     setFetchAgain={setFetchAgain}
-                                /> */}
+                                />
                             </>
                         ))}
                 </Text>
+                <Box d="flex" justifyContent="flex-end" p={3} bg="#E8E8E8">Messages here</Box>
             </React.Fragment>) : (
 
                 // to get socket.io on same page
