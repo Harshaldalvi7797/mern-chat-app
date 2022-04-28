5cc60e2429bcb7dc44a8cb8d2c9105e6853f401a
Harshaldalvi7797 <dalviharshal7797@gmail.com>
2022-04-28 16:17:36 +0530

    create group form ui

---
+++ frontend/src/components/miscellaneous/GroupChatModal.js
@@ -0,0 +1,119 @@
+import { useState } from "react"
+import axios from "axios";
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    useDisclosure,
+    FormControl,
+    Input,
+    useToast,
+    Box,
+} from "@chakra-ui/react";
+import { ChatState } from "../../Context/ChatProvider";
+import UserBadgeItem from "../userAvatar/UserBadgeItem";
+import UserListItem from "../userAvatar/UserListItem";
+const GroupChatModal = ({ children }) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [groupChatName, setGroupChatName] = useState();
+    const [selectedUsers, setSelectedUsers] = useState([]);
+    const [search, setSearch] = useState("");
+    const [searchResult, setSearchResult] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const toast = useToast();
+    const { user, chats, setChats } = ChatState();
+
+    const handleGroup = (userToAdd) => {
+        if (selectedUsers.includes(userToAdd)) {
+            toast({
+                title: "User already added",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "top",
+            });
+            return;
+        }
+
+        setSelectedUsers([...selectedUsers, userToAdd]);
+    };
+
+    const handleSearch = async (query) => { }
+
+
+    const handleDelete = (delUser) => {
+        setSelectedUsers(selectedUsers.filter((sel) => sel._id !== delUser._id));
+    };
+    const handleSubmit = async () => { }
+
+    return (
+        <>
+            <span onClick={onOpen}>{children}</span>
+
+            <Modal onClose={onClose} isOpen={isOpen} isCentered>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader
+                        fontSize="35px"
+                        fontFamily="Work sans"
+                        d="flex"
+                        justifyContent="center"
+                    >
+                        Create Group Chat
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody d="flex" flexDir="column" alignItems="center">
+                        <FormControl>
+                            <Input
+                                placeholder="Chat Name"
+                                mb={3}
+
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <Input
+                                placeholder="Add Users eg: John, Piyush, Jane"
+                                mb={1}
+
+                            />
+                        </FormControl>
+                        <Box w="100%" d="flex" flexWrap="wrap">
+                            {selectedUsers.map((u) => (
+                                <UserBadgeItem
+                                    key={u._id}
+                                    user={u}
+
+                                />
+                            ))}
+                        </Box>
+                        {loading ? (
+                            // <ChatLoading />
+                            <div>Loading...</div>
+                        ) : (
+                            searchResult
+                                ?.slice(0, 4)
+                                .map((user) => (
+                                    <UserListItem
+                                        key={user._id}
+                                        user={user}
+                                        handleFunction={() => handleGroup(user)}
+                                    />
+                                ))
+                        )}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button onClick={handleSubmit} colorScheme="blue">
+                            Create Chat
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    );
+};
+export default GroupChatModal;
